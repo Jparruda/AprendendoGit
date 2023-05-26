527287c92ab354fa862659ac6ef7047efe2ae757
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2023-05-26 19:46:29 -0300

    add tabela de multiplicação

---
+++ TabelaDeMultiplicação.py
@@ -0,0 +1,14 @@
+# teste = u"alo mundo"
+# print(teste)
+# print(type(teste))
+##Teste de while
+while True:
+    try:
+        a = 0
+        c = float(input("Coloque um número: "))
+        while a <= 9:
+            a = a+1
+            b = c*a
+            print(a,"*",c,'=',b)
+    except:
+        print("coloque um número real, de preferência")
